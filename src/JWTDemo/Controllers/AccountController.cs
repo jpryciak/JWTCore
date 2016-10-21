f496287aa8f40d935bee75c58524fe817030529d
jpryciak <jpryciak@gmail.com>
2016-10-21 16:22:42 -0400

    Add files via upload
    initial commit

---
+++ src/JWTDemo/Controllers/AccountController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JWTDemo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        private readonly SigningCredentials _signingCredentials;
+        private readonly JsonSerializerSettings _serializerSettings;
+
+
+        public AccountController()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("needtogetthisfromsomewheresafeandsecure"));
+            _signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            _serializerSettings = new JsonSerializerSettings { Formatting = Formatting.Indented };
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult ValidUserWithSeriousClaimsLogin()
+        {
+            const string validUserWithSeriousClaims = "Valid User With Serious Claims";
+            var claims = GetValidUserClaims(validUserWithSeriousClaims).ToList();
+
+            claims.Add(new Claim(ClaimTypes.Country, "This is SPARTA!", ClaimValueTypes.String, Issuer));
+
+            var claimsIdentity = new ClaimsIdentity("ValidUserWithSeriousClaimsIdentity");
+            claimsIdentity.AddClaims(claims);
+            return TokenResponse(claimsIdentity);
+
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult ValidUserWithoutAnySeriousClaimsLogin()
+        {
+            const string validUserWithoutAnySeriousClaims = "Valid User Without Any Serious Claims";
+            var claims = GetValidUserClaims(validUserWithoutAnySeriousClaims);
+
+            var claimsIdentity = new ClaimsIdentity("ValidUserWithoutAnySeriousClaimsIdentity");
+            claimsIdentity.AddClaims(claims);
+
+            return TokenResponse(claimsIdentity);
+
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult GenerateInvalidUserToken()
+        {
+            const string validUserWithoutAnySeriousClaims = "Token Signed with Invalid Crednetials";
+
+            var claims = GetValidUserClaims(validUserWithoutAnySeriousClaims);
+            var claimsIdentity = new ClaimsIdentity("TokenSignedWithInvalidCredentials");
+            claimsIdentity.AddClaims(claims);
+
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("someincorrectkey"));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            return TokenResponse(claimsIdentity, signingCredentials);
+        }
+
+        private IActionResult TokenResponse(ClaimsIdentity identity, SigningCredentials overridSigningCredentials = null)
+        {
+            var jwt = new JwtSecurityToken(
+                issuer: Issuer,
+                audience: Issuer,
+                claims: identity.Claims,
+                expires: DateTime.UtcNow.AddMinutes(10),
+                signingCredentials: overridSigningCredentials ?? _signingCredentials
+                );
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt
+            };
+
+            var json = JsonConvert.SerializeObject(response, _serializerSettings);
+            return new OkObjectResult(json);
+        }
+        private static IEnumerable<Claim> GetValidUserClaims(string validUserName)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, validUserName, ClaimValueTypes.String, Issuer),
+                new Claim(JwtRegisteredClaimNames.Jti, JtiGenerator, ClaimValueTypes.String, Issuer),
+                new Claim(ClaimTypes.Role, "ValidUsers", Issuer),
+                new Claim(ClaimTypes.Name, validUserName)
+            };
+            return claims;
+
+        }
+        private static string Issuer => "http://localhost:16137/";
+        private static string JtiGenerator => Guid.NewGuid().ToString();
+    }
+}
---
+++ src/JWTDemo/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace JWTDemo
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("OnlyValidUsers", 
+                policy =>
+                {
+                    policy.RequireRole("ValidUsers");
+                });
+                options.AddPolicy("ValidUserWithSeriousClaim", 
+                policy =>
+                {
+                    policy.RequireUserName("Valid user with Serious Claims");
+                    policy.RequireClaim(ClaimTypes.Country, "This is SPARTA!");
+
+                });
+
+                options.AddPolicy("ValidUserWithoutAnySeriousClaim",
+                    policy =>
+                    {
+                        policy.RequireUserName("Valid User Without Any Serious Claim");
+                    });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
